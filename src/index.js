fcb8d2ed8f731182da9310d6748c782318534827
jungyunhan <hanjy2008@naver.com>
2022-08-21 16:44:08 +0900

    github pages 배포를 위한 설정

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={'./src/index.js'}>
+    <BrowserRouter basename={'/Bulletin_Board'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
